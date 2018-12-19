ce2d6932485df980c93f94375d4826da268b7933
Thomas Munayyer <tmunayyer@gmail.com>
2018-12-19 13:35:44 -0500

    seed data ready

---
+++ exampleData.js
@@ -0,0 +1,40 @@
+const exampleData = [
+  {
+    //Movie_Id: 100,
+    Title: 'Bumblebee',
+    Theater: 'AMC Loews 34th Street 14',
+    Showtimes: {
+      Standard: ['1:30 PM', '2:30 PM'],
+      Imax: []
+    },
+    Theater_Details: {
+      Standard: [
+        'Closed Caption',
+        'Accessibility Devices Available',
+        'Dolby Cinema @ AMC',
+        'Recliner Seats',
+        'Reserved Seating'
+      ]
+    },
+    Trailer: {
+      Links: ['url1', 'url2'] //use youtube or something
+    },
+    Photos: {
+      Links: ['url1', 'url2'] //use a cache from somewhere, each on needs 10
+    },
+    Info: {
+      Description: 'long string',
+      Rating: 'PG-13 (for sequences of sci-fi action violence)',
+      Genre: 'Action & Adventure, Science Fiction & Fantasy',
+      Directed_By: 'Travis Knight',
+      Written_By: 'Christina Hodson',
+      ReleaseDate: ' Dec 21, 2018  wide',
+      Runtime: '114 minutes',
+      Studio: 'Paramount Pictures'
+    },
+    Cast: {
+      Hailee_Steinfeld: 'image url',
+      John_Cena: 'image url'
+    }
+  }
+];
