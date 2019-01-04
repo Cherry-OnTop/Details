cd8898797f8d8ec8af5488ab5cd051f52a5e1608
Thomas Munayyer <tmunayyer@gmail.com>
2019-01-04 14:24:22 -0500

    setting custom headers to get rid of random cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const cors = require('cors');
 const db = require('../db/index.js');
 const getMovie = require('./helper.js');
 const app = express();
 const port = 9002;
 
 app.use(express.static('./client/dist'));
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 //SERVER DATA
 app.get('/movie/:number', (req, res) => {
