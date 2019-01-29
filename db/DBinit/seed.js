4ae0ca30d7cffbee5fb65701f95da4aeef6363a6
paul lee <paul.plk@gmail.com>
2019-01-29 10:48:45 -0500

    dropped mongoose

--- db/DBinit/seed.js
+++ db/DBinit/seed.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
-const MovieSchema = require("../schema.js");
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/movies";
+var client = new MongoClient(url);
+const connection = client.connect();
 
 var movieData = fs.readFileSync("./data.json");
 var parsedMovieData = JSON.parse(movieData);
@@ -8,33 +11,38 @@ var count = 101;
 
 var assignNewMovieID = function() {
   for (var i = 0; i < parsedMovieData.length; i++) {
-    parsedMovieData[i]['_id'] = count;
-    parsedMovieData[i]['MovieId'] = count.toString();
+    parsedMovieData[i]["_id"] = count;
+    parsedMovieData[i]["MovieId"] = count.toString();
     count++;
   }
-  console.log('count:', count);
-}
+  console.log("count:", count);
+};
+
+const connect = connection;
 
 //splice dataToEnterTheDB into chunks to insert into DB
 var start = 1;
 var end = 1000;
 
-var storeBatch = () => {
-  assignNewMovieID();
-  MovieSchema.collection.insertMany(parsedMovieData, {ordered: false}, (err, data) => {
-    if (err) {
-      console.log(err)
-    } else if (start < end) {
-      start++;
-      storeBatch();
-    } else {
-      console.timeEnd("dbinsert");
-    }
-  });
-};
-
-console.time("dbinsert");
-storeBatch();
+connect.then(() => {
+  console.time("dbinsert");
+  var storeBatch = () => {
+    assignNewMovieID();
+    var db = client.db("movie");
+    const coll = db.collection("movies");
+    coll.insertMany(parsedMovieData, { ordered: false }, (err, data) => {
+      if (err) {
+        throw err;
+      } else if (start < end) {
+        start++;
+        storeBatch();
+      } else {
+        console.timeEnd("dbinsert");
+      }
+    });
+  };
+  storeBatch();
+});
 
 // in terminal, cd into DBinit
 // run following command
